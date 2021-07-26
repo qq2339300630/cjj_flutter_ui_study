f6bcf1f9f7d5da208357c9b7573dc52ad9d72eed
曹佳俊 <caojiajun1@shein.com>
2021-07-26 09:22:47 +0800

    附加上周五代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+
 
 // class CounterDisplay extends StatelessWidget {
 //   CounterDisplay({this.count});
@@ -51,6 +51,152 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+
+
+import 'package:flutter/material.dart';
+
+/**
+ * todo 自定义 appbar
+ * */
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          const IconButton(
+              onPressed: null,
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation menu'),
+          Expanded(child: title),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+              title: Text('Example title',
+                  style: Theme.of(context).primaryTextTheme.headline6)),
+          const Expanded(child: Center(child: Text('hello world')))
+        ],
+      ),
+    );
+  }
+}
+
+// void main() {
+//   runApp(MaterialApp(title: 'My app', home: SafeArea(child: MyScaffold())));
+// }
+
+/**
+ * todo 浮动按钮
+ * */
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: null,
+          tooltip: 'Navigation menu',
+        ),
+        title: const Text('Example title'),
+        actions: const <Widget>[
+          IconButton(
+              onPressed: null, icon: Icon(Icons.search), tooltip: 'Search')
+        ],
+      ),
+      body: const Center(
+        child: Text('hello world'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+    );
+  }
+}
+
+/**
+ * todo 简单点击事件
+ * */
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print("hello world");
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
+/**
+ *  todo 通用见面更新点击事件
+ * */
+
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(onPressed: _increment, child: Text('Increment')),
+        SizedBox(width: 16),
+        Text('Count: $_counter')
+      ],
+    );
+  }
+}
+
 
 class Product {
   Product({this.name});
